61c9866b65125a1676cf24ce19fa4bd4b12038ed
lllmoseslll <mosespeter620@gmail.com>
2024-07-31 01:36:41 +0300

    rename screens

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function index() {
+export default function CreatePost() {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Create Post</Text>
     </View>
   )
 }
