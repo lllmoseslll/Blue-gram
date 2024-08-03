682b93cb94aa02879ad92037576ac6e57cc5e0d8
lllmoseslll <mosespeter620@gmail.com>
2024-08-03 18:11:35 +0300

    add native wind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function index() {
   return (
-    <View>
-      <Text>Feeds Screen</Text>
+    <View className=" bg-slate-600 flex-1 justify-center items-center ">
+      <Text className="text-3xl font-bold text-blue-500">Feeds Screen</Text>
     </View>
-  )
+  );
 }
