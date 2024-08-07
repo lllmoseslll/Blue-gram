305d2f7dfec2fc79812f2e4368f0020796fa64aa
lllmoseslll <mosespeter620@gmail.com>
2024-08-07 03:36:30 +0300

    responsive feeds

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,16 @@
 import { View, Text, Image, FlatList, FlatListComponent } from "react-native";
 import React from "react";
 import posts from "~/assets/data/posts.json";
-import { AntDesign, Ionicons, Feather } from "@expo/vector-icons";
+// import { AntDesign, Ionicons, Feather } from "@expo/vector-icons";
 import PostListItem from "~/src/component/PostListItem";
 
 export default function FeedScreen() {
   return (
     <FlatList
+      className="max-w-xl self-center "
       data={posts}
       renderItem={({ item }) => <PostListItem post={item} />}
-      contentContainerStyle={{ gap: 10 }}
+      contentContainerStyle={{ gap: 10, maxWidth: 512, width: "100%" }}
       showsVerticalScrollIndicator={false}
     />
   );
