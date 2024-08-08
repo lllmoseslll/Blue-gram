501b3f2525582e6d96e2985a7f9ab9aa676ba79a
lllmoseslll <mosespeter620@gmail.com>
2024-08-08 16:30:11 +0300

    profile page

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,19 +1,46 @@
 import { View, Text, Image, TextInput, Pressable } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import Button from "~/src/component/Button";
 
 export default function CreatePost() {
   const [caption, setCaption] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [5, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <View className="p-3 items-center flex-1 ">
       {/* image picker */}
-      <Image
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/4.jpg",
-        }}
-        className="w-52 aspect-[3/4]  rounded-lg shadow-md bg-slate-300"
-      />
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          className="w-52 aspect-[3/4]  rounded-lg shadow-md bg-slate-300"
+        />
+      ) : (
+        <View className="w-52 aspect-[3/4]  rounded-lg shadow-md bg-slate-300" />
+      )}
 
-      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+      <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
         Change
       </Text>
 
@@ -27,11 +54,11 @@ export default function CreatePost() {
 
       {/* button */}
       <View className="mt-auto w-full">
-          <Pressable className="bg-blue-500 w-full p-3 items-center rounded-md">
-        <Text className=" text-white font-semibold">Share Post</Text>
-      </Pressable>  
+        {/* <Pressable className="bg-blue-500 w-full p-3 items-center rounded-md">
+          <Text className=" text-white font-semibold">Share Post</Text>
+        </Pressable> */}
+        <Button title={"Share Post"} />
       </View>
-  
     </View>
   );
 }
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import Button from "~/src/component/Button";
 
 export default function ProfileScreen() {
+  const [image, setImage] = useState<string | null>(null);
+  const [username, setUsername] = useState("");
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View className="p-5 flex-1">
+      {/* avator image picker */}
+
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          className="w-52 aspect-square self-center rounded-full shadow-md bg-slate-300"
+        />
+      ) : (
+        <View className="w-52 aspect-square self-center rounded-full shadow-md bg-slate-300" />
+      )}
+      <Text
+        onPress={pickImage}
+        className="text-blue-500 font-semibold self-center m-5"
+      >
+        Change
+      </Text>
+
+      {/* form */}
+      <Text className="mb-2 text-gray-500 font-semibold">Username</Text>
+      <TextInput
+        placeholder="username"
+        value={username}
+        onChangeText={setUsername}
+        className="border border-gray-300 p-3 rounded-md shadow-lg"
+      />
+      {/* button */}
+      <View className="gap-3 mt-[90%]">
+        <Button title="Update Profile" />
+        <Button title="Sign Out" />
+      </View>
     </View>
-  )
+  );
 }
